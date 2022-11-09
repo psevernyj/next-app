b74c88d2da8b627307f6a19da8c24f0b48cd1a5e
Anastasiya Muzychenko <an.muzychenko@uds.systems>
2022-11-09 15:57:00 +0200

    Added services section to Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 import partnership from "../assets/home-page/header_microsoft_partner_logo.webp";
 import gearIcon from "../assets/home-page/gear_icon.webp";
+import React from "react";
 
 const RootComponent: NextPage = () => {
     return (
@@ -58,8 +59,8 @@ const RootComponent: NextPage = () => {
                         </li>
                     </ul>
                 </div>
-                <div className={"services"}>
-                    <div className={"container section"}>
+                <div className={"services container"}>
+                    <div className={"section"}>
                         <h2 className={"pageTitle"}>Our Services</h2>
                         <p className={"pageSubtitle"}>
                             We provide services directly to end users or collaborate with Microsoft partners as
@@ -68,7 +69,7 @@ const RootComponent: NextPage = () => {
                         </p>
                         <div className={"serviceCases"}>
                             <div className={"service"}>
-                                <Image src={gearIcon} alt={"Icon"} width={44} height={44}/>
+                                <Image className={"gearIcon"} src={gearIcon} alt={"Icon"} width={44} height={44}/>
                                 <p className={"title"}>Consulting</p>
                                 <p className={"subtitle"}>
                                     Want to fully benefit from the Dynamics 365?
@@ -76,7 +77,7 @@ const RootComponent: NextPage = () => {
                                 </p>
                             </div>
                             <div className={"service"}>
-                                <Image src={gearIcon} alt={"Icon"} width={44} height={44}/>
+                                <Image className={"gearIcon"} src={gearIcon} alt={"Icon"} width={44} height={44}/>
                                 <p className={"title"}>Integration & Implementation</p>
                                 <p className={"subtitle"}>
                                     Want to rationally implement Dynamics 365 for business automation? The UDS complex
@@ -84,7 +85,7 @@ const RootComponent: NextPage = () => {
                                 </p>
                             </div>
                             <div className={"service"}>
-                                <Image src={gearIcon} alt={"Icon"} width={44} height={44}/>
+                                <Image className={"gearIcon"} src={gearIcon} alt={"Icon"} width={44} height={44}/>
                                 <p className={"title"}>Development & Customization</p>
                                 <p className={"subtitle"}>
                                     Need smart tools for performing highly personalized tasks and improving Dynamics 365
@@ -92,13 +93,25 @@ const RootComponent: NextPage = () => {
                                 </p>
                             </div>
                             <div className={"service"}>
-                                <Image src={gearIcon} alt={"Icon"} width={44} height={44}/>
+                                <Image className={"gearIcon"} src={gearIcon} alt={"Icon"} width={44} height={44}/>
                                 <p className={"title"}>Support & Management</p>
                                 <p className={"subtitle"}>
                                     Need a team of professionals to maintain your Dynamics 365?
                                     We offer you our best personnel and rich technical background.
                                 </p>
                             </div>
+                        </div>
+                        <div className={"serviceAction"}>
+                            <p className={"subtitle"}>
+                                Interested? <br/>
+                                Let us know how we can help you.
+                            </p>
+                            <button className={"button orangeButton bigButton"}>
+                                Talk with consultant
+                                <svg width="17" height="23" viewBox="0 0 17 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M13.5449 1.22317L12.3292 0.842089C10.9516 0.410233 9.47974 1.10815 8.88979 2.47297L7.95055 4.64583C7.4477 5.80915 7.71269 7.17632 8.60963 8.04618L10.1662 9.55576C10.2117 9.59988 10.2405 9.65948 10.2474 9.72377C10.2936 10.154 10.0132 10.9919 9.36705 12.1557C8.8972 13.0021 8.47195 13.5956 8.10997 13.9294C7.85778 14.162 7.71896 14.2123 7.65985 14.194L5.56578 13.5282C4.39328 13.1554 3.12265 13.5999 2.40524 14.634L1.07115 16.5569C0.231447 17.7672 0.38245 19.4369 1.42441 20.4632L2.3477 21.3725C3.34398 22.3538 4.76225 22.7194 6.08388 22.3358C8.95272 21.503 11.5249 18.9852 13.829 14.8348C16.1362 10.6787 16.9458 7.09893 16.1995 4.09616C15.8579 2.72197 14.8515 1.63279 13.5449 1.22317ZM13.0939 2.77902C13.8779 3.0248 14.4818 3.6783 14.6867 4.50282C15.3144 7.02863 14.5969 10.2016 12.4758 14.0223C10.3575 17.838 8.07737 20.07 5.66381 20.7706C4.87083 21.0008 4.01987 20.7814 3.4221 20.1926L2.49882 19.2833C2.0252 18.8168 1.95656 18.0578 2.33824 17.5077L3.67234 15.5848C3.99843 15.1148 4.57599 14.9127 5.10894 15.0822L7.20826 15.7496C8.42671 16.1274 9.53262 15.1075 10.7202 12.9682C11.5205 11.5266 11.8935 10.4122 11.8002 9.5434C11.7519 9.09335 11.5503 8.67615 11.2318 8.36731L9.67525 6.85773C9.26755 6.46234 9.1471 5.8409 9.37567 5.31212L10.3149 3.13925C10.5831 2.51888 11.2521 2.20165 11.8783 2.39794L13.0939 2.77902Z" fill="white"/>
+                                </svg>
+                            </button>
                         </div>
                     </div>
 
