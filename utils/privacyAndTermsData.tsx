3cc9565ad79c00ad82754c8e43fc1e35c870f68b
Anastasiya <psevernyj69@gmail.com>
2022-11-29 13:53:32 +0200

    Added new block to privacy page

---
+++ utils/privacyAndTermsData.tsx
@@ -0,0 +1,126 @@
+export const privacyItems = [
+    {
+        title: "UDS Systems may be contacted as follows:",
+        content: <div className={"addressContent"}>
+            <div className={"address"}>
+                <div className={"item"}>
+                    UDS SYSTEMS LTD
+                </div>
+                <div className={"item"}>
+                    Unit 7118, Second Floor,
+                    6 Market Place
+                </div>
+                <div className={"item"}>
+                    London, Fitzrovia,
+                    United Kingdom, W1W 8AF
+                </div>
+                <div className={"item"}>
+                    <a href={"mailto:info@uds.systems"} className={"link"} aria-label={"Our mail"}>
+                        info@uds.systems
+                    </a>
+                </div>
+                <div className={"item"}>
+                    <a href={"mailto:portal@uds.systems"} className={"link"} aria-label={"Our mail"}>
+                        portal@uds.systems
+                    </a>
+                </div>
+            </div>
+            <div className={"paragraphSection"}>
+                <p className={"paragraph"}>
+                    This Privacy Policy ("Privacy Policy") applies to UDS Systems as well as website <a
+                    href={"https://uds.systems/"} className={"orangeLink"}>https://uds.systems/</a> or <a
+                    href={"https://my.uds.systems/"}
+                    className={"orangeLink"}>https://my.uds.systems/</a> (hereinafter, "us", "we", “Website”). We
+                    respect your privacy and are committed
+                    to protecting personal data you may
+                    provide us through the Website. We have adopted this Privacy Policy to explain what information
+                    may be collected on our
+                    Website, how, and under what conditions we collect and process this information as well as
+                    conditions of its disclosure to third parties. This Privacy Policy applies only to information
+                    we collect through the Website and does not apply to our collection of information from other
+                    sources.
+                </p>
+                <p className={"paragraph"}>
+                    This Privacy Policy, together with the Terms and conditions posted on our Website, set forth the
+                    general rules and policies governing your use of our Website. When purchasing additional
+                    services, you may be required to agree to additional terms and conditions.
+                </p>
+                <p className={"paragraph"}>
+                    We may from time to time revise this Privacy Policy, inter alia, to comply with applicable laws.
+                    While we shall do our best to inform you of any changes, it is your duty to review this Privacy
+                    Policy from time to time.
+                </p>
+
+            </div>
+        </div>
+    },
+    {
+        title: "Lawful collection and processing",
+        content: <div className={"paragraphSection"}>
+            <p className={"paragraph"}>
+                You are not required to provide us your personal data, but without adequate information we will not be
+                able to provide you with some of our services.
+            </p>
+            <p className={"paragraph"}>
+                We act as “data controller” within the meaning of the General Data Protection Regulation (EU) 2016/679
+                (hereinafter referred to as “GDPR”) and other applicable European laws pertaining to data protection.
+            </p>
+            <p className={"paragraph"}>
+                We process your personal data only if at least one of the following applies:
+            </p>
+            <ul className={"list"}>
+                <li className={"item"}>
+                    you have given consent to the processing of your personal data;
+                </li>
+                <li className={"item"}>
+                    processing is necessary for entering into or the execution of an agreement with you or in order to
+                    render you with products and services;
+                </li>
+                <li className={"item"}>
+                    processing is necessary for compliance with applicable legislation.
+                </li>
+            </ul>
+
+        </div>
+    },
+    {
+        title: "Objectives of processing personal data",
+        content: "We render products or services relating to easing operation of and access to third party services, including but not limited to Microsoft Dynamics 365, PowerApps and Azure. We require your personal data in order to render services to:"
+    },
+    {
+        title: "Processed personal data",
+        content: "We render products or services relating to easing operation of and access to third party services, including but not limited to Microsoft Dynamics 365, PowerApps and Azure. We require your personal data in order to render services to:"
+    },
+    {
+        title: "Disclosure of your personal data to third parties",
+        content: "test"
+    },
+    {
+        title: "Personal data storage",
+        content: "test"
+    },
+    {
+        title: "Rights of personal data subjects",
+        content: "test"
+    },
+    {
+        title: "Security",
+        content: "test"
+    },
+    {
+        title: "Cookies",
+        content: "test"
+    },
+    {
+        title: "Advertisements",
+        content: "test"
+    },
+    {
+        title: "Links to external sites",
+        content: "test"
+    },
+    {
+        title: "Affiliate Disclosure",
+        content: "test"
+    },
+]
