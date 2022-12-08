2eee785f7a3e55ab213a637c8f018ab09a623f81
Anastasiya Muzychenko <an.muzychenko@uds.systems>
2022-12-08 19:24:47 +0200

    Added new block to privacy policy

--- utils/privacyAndTermsData.tsx
+++ utils/privacyAndTermsData.tsx
@@ -50,7 +50,6 @@ export const privacyItems = [
                     While we shall do our best to inform you of any changes, it is your duty to review this Privacy
                     Policy from time to time.
                 </p>
-
             </div>
         </div>
     },
@@ -80,20 +79,196 @@ export const privacyItems = [
                     processing is necessary for compliance with applicable legislation.
                 </li>
             </ul>
-
         </div>
     },
     {
         title: "Objectives of processing personal data",
-        content: "We render products or services relating to easing operation of and access to third party services, including but not limited to Microsoft Dynamics 365, PowerApps and Azure. We require your personal data in order to render services to:"
+        content: <div className={"paragraphSection"}>
+            <p className={"paragraph"}>
+                We render products or services relating to easing operation of and access to third party services,
+                including but not limited to Microsoft Dynamics 365, PowerApps and Azure. We require your personal data
+                in order to render services to:
+            </p>
+            <ul className={"list"}>
+                <li className={"item"}>
+                    facilitate your registration;
+                </li>
+                <li className={"item"}>
+                    provide you with the whole range of our services;
+                </li>
+                <li className={"item"}>
+                    update and adjust your user profile;
+                </li>
+                <li className={"item"}>
+                    furnish you with crucial information related to services;
+                </li>
+                <li className={"item"}>
+                    have a possibility to familiarize you with all our special offers and other services;
+                </li>
+                <li className={"item"}>
+                    provide user support;
+                </li>
+                <li className={"item"}>
+                    process your complaints and suggestions;
+                </li>
+                <li className={"item"}>
+                    ensure lawfulness of rendered services, including but not limited Know-Your-Client,
+                    Anti-Money-Laundry checks and international sanctions compliance;
+                </li>
+                <li className={"item"}>
+                    improve our services, including questionnaires on satisfaction with these services;
+                </li>
+                <li className={"item"}>
+                    statistical and marketing analysis of utilization of our services.
+                </li>
+            </ul>
+            <p className={"paragraph"}>
+                We ask you to inform us only about such personal data that are required to render the service or
+                newsletters you chose, or a react to your request/claim. At the same time, if you decide to furnish us
+                with any additional personal data, we will be able to process them with due level of protection.
+            </p>
+        </div>
     },
     {
         title: "Processed personal data",
-        content: "We render products or services relating to easing operation of and access to third party services, including but not limited to Microsoft Dynamics 365, PowerApps and Azure. We require your personal data in order to render services to:"
+        content: <div className={"paragraphSection"}>
+            <p className={"paragraph"}>
+                Please see the table below to find out which of your personal data we process to provide your services
+                to you:
+            </p>
+            <div className={"twoColumn"}>
+                <ul className={"list"}>
+                    <li className={"item"}>
+                        full name;
+                    </li>
+                    <li className={"item"}>
+                        date of birth;
+                    </li>
+                    <li className={"item"}>
+                        language;
+                    </li>
+                    <li className={"item"}>
+                        nationality;
+                    </li>
+                    <li className={"item"}>
+                        company;
+                    </li>
+                    <li className={"item"}>
+                        position;
+                    </li>
+                </ul>
+                <ul className={"list"}>
+                    <li className={"item"}>
+                        e-mail;
+                    </li>
+                    <li className={"item"}>
+                        telephone number (-s);
+                    </li>
+                    <li className={"item"}>
+                        payment details;
+                    </li>
+                    <li className={"item"}>
+                        billing address;
+                    </li>
+                    <li className={"item"}>
+                        information about services you used;
+                    </li>
+                    <li className={"item"}>
+                        technical information, which by itself is not personal, but jointly may constitute personal
+                        data, for example: IP number, Cookies, logs, browser type, language preference, referring site,
+                        and the date and time of each visitor request etc.
+                    </li>
+                </ul>
+            </div>
+        </div>
     },
     {
         title: "Disclosure of your personal data to third parties",
-        content: "test"
+        content: <div className={"paragraphSection"}>
+            <p className={"paragraph"}>
+                In order to enable you to use our services, we must transfer your personal data to third parties. Many
+                of those are abroad, therefore disclosure to parties abroad is deemed international data transfer. Third
+                party processors of personal data are as follows:
+            </p>
+            <table className="thirdPartiesTable">
+                <thead>
+                <tr>
+                    <th>Company name</th>
+                    <th>Address</th>
+                    <th>Rendered Service</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td valign="top" aria-label="Company name">UDS Systems East European Regional Office</td>
+                    <td valign="top" aria-label="Address">Office 245,
+                        17/31A Kolomyiskyi Lane, Kyiv, 03022, Ukraine
+                    </td>
+                    <td valign="top" aria-label="Rendered Service">Marketing, End-user support, systems upkeep</td>
+                </tr>
+                <tr>
+                    <td valign="top" aria-label="Company name">Microsoft Deutschland GmbH</td>
+                    <td valign="top" aria-label="Address">Konrad-Zuse-Str.1, 85716 Unterschleißheim, Germany</td>
+                    <td valign="top" aria-label="Rendered Service">Primary service provider, hosting provider, email
+                        provider, OpenID
+                    </td>
+                </tr>
+                <tr>
+                    <td valign="top" aria-label="Company name">Azure Active Directory B2C</td>
+                    <td valign="top" aria-label="Address">Redmond, Washington 98052, USA</td>
+                    <td valign="top" aria-label="Rendered Service">Identity management service</td>
+                </tr>
+                <tr>
+                    <td valign="top" aria-label="Company name">Azure BLOB Storage</td>
+                    <td valign="top" aria-label="Address">Redmond, Washington 98052, USA</td>
+                    <td valign="top" aria-label="Rendered Service">Azure service</td>
+                </tr>
+                <tr>
+                    <td valign="top" aria-label="Company name">DNS Made Easy / Tiggee LLC</td>
+                    <td valign="top" aria-label="Address">11490 Commerce Park Drive Suite #140, Reston VA 20191, USA
+                    </td>
+                    <td valign="top" aria-label="Rendered Service">DNS management services</td>
+                </tr>
+                <tr>
+                    <td valign="top" aria-label="Company name">Hotjar Ltd</td>
+                    <td valign="top" aria-label="Address">Dragonara Business Centre, 5th Floor, Dragonara Road,
+                        Paceville St Julian's STJ 3141, Malta
+                    </td>
+                    <td valign="top" aria-label="Rendered Service">A heatmap, survey, feedback and funnel application.
+                    </td>
+                </tr>
+                <tr>
+                    <td valign="top" aria-label="Company name">Leadfeeder / Liidio Oy</td>
+                    <td valign="top" aria-label="Address">Mikonkatu 17 C, 00100 Helsinki, Finland</td>
+                    <td valign="top" aria-label="Rendered Service">Marketing information.</td>
+                </tr>
+                <tr>
+                    <td valign="top" aria-label="Company name">Google Ireland Limited</td>
+                    <td valign="top" aria-label="Address">Gordon House, Barrow St, Dublin 4, Ireland</td>
+                    <td valign="top" aria-label="Rendered Service">Marketing information, Analytics, Conversion
+                        Tracking,
+                        mobile content optimization, Advertising, OpenID
+                    </td>
+                </tr>
+                <tr>
+                    <td valign="top" aria-label="Company name">Facebook Ireland Ltd</td>
+                    <td valign="top" aria-label="Address">4 Grand Canal Square Grand Canal Harbour Dublin 2, Ireland
+                    </td>
+                    <td valign="top" aria-label="Rendered Service">Marketing information, Advertising, Conversion
+                        Tracking, Integrations, OpenID
+                    </td>
+                </tr>
+                <tr>
+                    <td valign="top" aria-label="Company name">LinkedIn Ireland Unlimited Company</td>
+                    <td valign="top" aria-label="Address">Wilton Plaza, Gardner House 4,5,6 2 Dublin, Ireland</td>
+                    <td valign="top" aria-label="Rendered Service">Marketing information, Analytics, Conversion
+                        Tracking.
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+
+        </div>
     },
     {
         title: "Personal data storage",
